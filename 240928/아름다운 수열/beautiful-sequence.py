d5e26f4f529770acf6f767310357dbcc53d10cff
nicebin23 <127166082+nicebin23@users.noreply.github.com>
2024-09-28 01:10:38 +0900

    Title: 아름다운 수열, Time: 185ms, Memory: 26MB, Status: Passed - CodeTree

--- 240928/아름다운 수열/beautiful-sequence.py
+++ 240928/아름다운 수열/beautiful-sequence.py
@@ -1,32 +1,35 @@
+def bSort(arr) :
+  n = len(arr)
+  for last in range(1, n) :
+    for i in range(last, 0, -1) :
+      if arr[i-1] > arr[i] :
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+  return arr
+
 def check_beautiful_sequences(A, B):
     N = len(A)
     M = len(B)
     results = []
-
-    min_B = min(B)
-    max_B = max(B)
+    count = 0
+    
+    B_sorted = bSort(B)
 
     for i in range(N - M + 1):
         window = A[i:i + M]
-        window_set = set(window)
-
-        for k in range(-max_B, max_B + 1):
-            transformed_B = {b + k for b in B}
-            if transformed_B.issubset(window_set):
-                results.append(i)
-                break
-
-    return results
-
+        window_sorted = bSort(window)
+        diff = [window_sorted[j] - B_sorted[j] for j in range(M)]
+        if len(set(diff)) == 1:
+            results.append(i + 1)
+            count += 1
+    return count, results
 
 N = int(input())
 A = [int(input()) for i in range(N)]
-
 M = int(input())
 B = [int(input()) for i in range(M)]
 
-result = check_beautiful_sequences(A, B)
-print(len(result))
+count, beautiful_indices = check_beautiful_sequences(A, B)
 
-for index in result :
-    print(index+1)
+print(count)
+for index in beautiful_indices:
+    print(index)
